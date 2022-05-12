ed282a58f7bd79469f0d761b2f1d5520d88864f0
Laressa Moraes <laressa.luisy@hotmail.com>
2022-05-12 16:28:41 -0300

    Getting a user by id

--- src/main/java/com/grupowl/springbootproject/services/UserService.java
+++ src/main/java/com/grupowl/springbootproject/services/UserService.java
@@ -1,12 +1,14 @@
 package com.grupowl.springbootproject.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grupowl.springbootproject.domain.User;
 import com.grupowl.springbootproject.repository.UserRepository;
+import com.grupowl.springbootproject.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
