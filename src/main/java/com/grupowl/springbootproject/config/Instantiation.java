8edbdfe3c10d5383d55934aeac6cd06081c24bfd
Laressa Moraes <laressa.luisy@hotmail.com>
2022-05-12 16:05:45 -0300

    Database instantiation operation

---
+++ src/main/java/com/grupowl/springbootproject/config/Instantiation.java
@@ -0,0 +1,29 @@
+package com.grupowl.springbootproject.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.grupowl.springbootproject.domain.User;
+import com.grupowl.springbootproject.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userReposiroty;
+
+	@Override
+	public void run(String... arg0) throws Exception {
+
+		userReposiroty.deleteAll();
+
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+	}
+}
