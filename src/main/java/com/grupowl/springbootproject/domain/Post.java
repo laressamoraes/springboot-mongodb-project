b956942600281c3869b8fff3986e18b02c624096
Laressa Moraes <laressa.luisy@hotmail.com>
2022-05-12 17:37:29 -0300

    Adding comments to posts

--- src/main/java/com/grupowl/springbootproject/domain/Post.java
+++ src/main/java/com/grupowl/springbootproject/domain/Post.java
@@ -1,12 +1,15 @@
 package com.grupowl.springbootproject.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.grupowl.springbootproject.dto.AuthorDTO;
+import com.grupowl.springbootproject.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
